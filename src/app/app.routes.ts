19793e95d592f8f4c8b9d728b4e85feaa620b5f3
vinhtt <vinhtt@tringhiatech.vn>
2025-02-12 17:30:52 +0700

    upload mobile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TrangChuComponent } from './trang-chu/trang-chu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'trang-chu', component: TrangChuComponent },
+    { path: '', component: TrangChuComponent },
+];
---
+++ src/app/trang-chu/trang-chu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trang-chu',
+  imports: [],
+  templateUrl: './trang-chu.component.html',
+  styleUrl: './trang-chu.component.css'
+})
+export class TrangChuComponent {
+
+}
